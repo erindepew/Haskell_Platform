4e81669602b7be54c122f9d7427a5239c3b424e4
Erin Depew <erinldepew@gmail.com>
2014-09-06 16:24:31 -0400

    FINALLY got breakpoint mixin working

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'compass'
+require 'breakpoint'
+require 'toolkit'
+
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
