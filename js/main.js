3235ba8cc1fa22e0834b670abcc9ef4b29abca6e
Erin Depew <erinldepew@gmail.com>
2014-10-04 17:35:02 -0400

    fixed fonts and navigation

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@
 
 $(function() {
     $(".navigation--mobile-open").click(function () {
-        $("#navigation--MOBILE").animate({"left": "0"}, "slow");
+        //$("#navigation--MOBILE").animate({"left": "0"}, "slow");
+        $(".site-wrapper").animate({"left": "13em"}, "slow");
         $(".navigation--mobile-open").css("display", "none");
         $(".navigation--mobile-close").css("display", "block");
     });
 
     $(".navigation--mobile-close").click(function () {
-        $("#navigation--MOBILE").animate({"left": "-13em"}, "slow");
+        //$("#navigation--MOBILE").animate({"left": "-13em"}, "slow");
+        $(".site-wrapper").animate({"left": "0"}, "slow");
         $(".navigation--mobile-open").css("display", "block");
         $(".navigation--mobile-close").css("display", "none");
     });
 
 });
+
